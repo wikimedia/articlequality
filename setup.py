c84c669265fa9a507f1cee019f980c128480750b
halfak <aaron.halfaker@gmail.com>
2014-06-11 10:05:20 -0500

    Added setup.py, VERSION and a MANIFEST.  setup.py install works.

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def requirements(fname):
+    for line in open(os.path.join(os.path.dirname(__file__), fname)):
+        yield line.strip()
+
+setup(
+    name = "wikiclass",
+    version = read('VERSION').strip(),
+    author = "Aaron Halfaker / Morten Warncke-Wang",
+    author_email = "ahalfaker@wikimedia.org",
+    description = ("A library for performing automatic detection of assessment classes of Wikipedia articles."),
+    license = "MIT",
+    url = "https://github.com/halfak/Wiki-Class",
+    py_modules = ['wikiclass'],
+    long_description = read('README.rst'),
+    install_requires = requirements('requirements.txt'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Text Processing :: General",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering"
+    ],
+)
--- wikiclass/languages/language.py
+++ wikiclass/languages/language.py
@@ -2,26 +2,26 @@
 LANGUAGES = {}
 
 class Language:
-	
-	def __init__(self, name, stopwords, stemmer):
-		self.name = str(name)
-		self.stopwords = set(stopwords)
-		self.stemmer = stemmer
-	
-	def filter_stopwords(self, words):
-		for word in words:
-			if word.lower() not in self.stopwords:
-				yield word
-	
-	def stem_words(self, words):
-		for word in words:
-			try:
-				yield stemmer.stem(word)
-			except:
-				continue
+    
+    def __init__(self, name, stopwords, stemmer):
+        self.name = str(name)
+        self.stopwords = set(stopwords)
+        self.stemmer = stemmer
+    
+    def filter_stopwords(self, words):
+        for word in words:
+            if word.lower() not in self.stopwords:
+                yield word
+    
+    def stem_words(self, words):
+        for word in words:
+            try:
+                yield stemmer.stem(word)
+            except:
+                continue
 
 def get(lang_name):
-	return LANGUAGES[lang_name]
+    return LANGUAGES[lang_name]
 
 def register(language):
-	LANGUAGES[language.name] = language
+    LANGUAGES[language.name] = language
