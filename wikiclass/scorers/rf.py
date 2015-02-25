e603fe930c2d94d69f96abad1affd5a628c2ba85
halfak <aaron.halfaker@gmail.com>
2015-02-24 19:40:01 -0600

    Adds tests for all features and datasources.  Minor fixes to rf and train_test too.

--- wikiclass/scorers/rf.py
+++ wikiclass/scorers/rf.py
@@ -1,15 +1,22 @@
+import logging
+
 from revscoring.scorers import ScikitLearnClassifier
 from sklearn.ensemble import RandomForestClassifier
-
-from .. import assessments, languages
-from ..features import TextFeatureExtractor, WikitextAndInfonoise
-from .model import ModelFile, TextModel
 
 logger = logging.getLogger("wikiclass.scorers.rf_text")
 
 class RFModel(ScikitLearnClassifier):
     
     def __init__(self, features, *, language=None, rf=None, **kwargs):
+        
+        if 'n_estimators' not in kwargs:
+            kwargs['n_estimators'] = 501
+        
+        if 'min_samples_leaf' not in kwargs:
+            kwargs['min_samples_leaf'] = 8
+        
+        if 'criterion' not in kwargs:
+            kwargs['criterion'] = 'entropy'
         
         if rf is None: rf = RandomForestClassifier(**kwargs)
         
