aa4edc1ea84ef392c1abdfdac8ddd6786aba67de
halfak <aaron.halfaker@gmail.com>
2015-10-31 13:58:30 -0500

    Adds parallelization to extract_features

--- wikiclass/utilities/extract_features.py
+++ wikiclass/utilities/extract_features.py
@@ -15,22 +15,26 @@
         extract_features -h | --help
         extract_features <features> [--labelings=<path>]
                                     [--value-labels=<path>]
+                                    [--extractors=<num>]
                                     [--verbose]
                                     [--debug]
 
     Options:
-        -h --help                Print this documentation
-        <features>               Classpath to a list/tuple of features
-        --labelings=<path>       Path to a file containing labeling docs pairs
-                                 [default: <stdin>]
-        --value-labels=<path>    Path to a file to write feature value-labels to
-                                 [default: <stdout>]
-        --verbose                Print dots and stuff to stderr
-        --debug                  Print debug logs
+        -h --help               Print this documentation
+        <features>              Classpath to a list/tuple of features
+        --labelings=<path>      Path to a file containing labeling docs pairs
+                                [default: <stdin>]
+        --value-labels=<path>   Path to a file to write feature value-labels to
+                                [default: <stdout>]
+        --extractors=<num>      The number of parallel extractors to
+                                start [default: <cpu count>]
+        --verbose               Print dots and stuff to stderr
+        --debug                 Print debug logs
 """
 import json
 import logging
 import sys
+from multiprocessing import Pool, cpu_count
 
 import docopt
 from revscoring.datasources import revision
@@ -58,25 +62,52 @@ def main(argv=None):
     else:
         value_labels = sys.stdout
 
+    if args['--extractors'] == "<cpu count>":
+        extractors = cpu_count()
+    else:
+        extractors = int(args['--extractors'])
+
     verbose = args['--verbose']
 
-    run(labelings, features, solve, value_labels, verbose)
+    run(labelings, features, solve, value_labels, extractors, verbose)
 
 
-def run(labelings, features, solve, value_labels, verbose=False):
+def run(labelings, features, solve, value_labels, extractors, verbose=False):
+    extractor_pool = Pool(processes=extractors)
 
-    for labeling in labelings:
-        feature_values = extract_features(features, labeling['text'])
-        if verbose:
-            sys.stderr.write(".")
-            sys.stderr.flush()
+    extractor = LabelingFeatureExtractor(features)
 
-        value_labels.write("\t".join([encode(v) for v in feature_values] +
-                                     [labeling['label']]))
-        value_labels.write("\n")
+    feature_labels = extractor_pool.imap(extractor.extract, labelings)
+    for feature_values, label in feature_labels:
+        if feature_values is not None:
+            if verbose:
+                sys.stderr.write(".")
+                sys.stderr.flush()
+            value_labels.write("\t".join([encode(v) for v in feature_values] +
+                                         [label]))
+            value_labels.write("\n")
+        else:
+            if verbose:
+                sys.stderr.write("-")
+                sys.stderr.flush()
 
     if verbose:
         sys.stderr.write("\n")
+
+
+class LabelingFeatureExtractor:
+
+    def __init__(self, features):
+        self.features = features
+
+    def extract(self, labeling):
+        if labeling['text'] is None:
+            return None
+
+        feature_values = extract_features(self.features, labeling['text'])
+
+        return list(feature_values), labeling['label']
+
 
 def extract_features(features, text, cache=None, context=None):
     """
