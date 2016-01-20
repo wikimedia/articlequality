cf14f380c2e733dfa6e7661e9b71f111383d9f0e
halfak <aaron.halfaker@gmail.com>
2016-01-20 16:06:19 -0600

    Updates enwiki and frwiki feature_lists for revscoring 1.0

---
+++ wikiclass/feature_lists/wikipedia.py
@@ -0,0 +1,17 @@
+from revscoring.features import wikitext
+
+article = [
+    wikitext.revision.content_chars,
+    wikitext.revision.ref_tags,
+    wikitext.revision.ref_tags / max(wikitext.revision.content_chars, 1),
+    wikitext.revision.wikilinks,
+    wikitext.revision.wikilinks / max(wikitext.revision.content_chars, 1),
+    wikitext.revision.external_links,
+    wikitext.revision.external_links / max(wikitext.revision.content_chars, 1),
+    wikitext.revision.headings_by_level(2),
+    wikitext.revision.headings_by_level(2) /
+        max(wikitext.revision.content_chars, 1),
+    wikitext.revision.headings_by_level(3),
+    wikitext.revision.headings_by_level(3) /
+        max(wikitext.revision.content_chars, 1)
+]
