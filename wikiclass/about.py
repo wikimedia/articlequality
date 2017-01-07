851957ec3823cd769dee3796b4e35364242f0737
halfak <aaron.halfaker@gmail.com>
2017-01-07 12:46:05 -0600

    Increments version to 0.3.2

--- wikiclass/about.py
+++ wikiclass/about.py
@@ -1,5 +1,5 @@
 __name__ = "wikiclass"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "Aaron Halfaker / Morten Warncke-Wang"
 __author_email__ = "ahalfaker@wikimedia.org"
 __description__ = "A library for performing automatic detection of " + \
