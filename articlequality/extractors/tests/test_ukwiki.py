fbc4ce5d5962188d7c26308b2256567e5049b76d
Chaitanya Mittal <chtnnh@gmail.com>
2020-05-29 18:15:19 +0400

    Add revision templates to extractors/tests/test_ukwiki.py

--- articlequality/extractors/tests/test_ukwiki.py
+++ articlequality/extractors/tests/test_ukwiki.py
@@ -18,7 +18,20 @@ def test_extractor():
         def __iter__(self):
             return iter(self.revisions)
 
-    revisions = []
+    revisions = [
+        Revision(
+            1, Timestamp(1), "aaa",
+            "{{Проект:Адміністративні одиниці країн світу|Польща}}"
+        ),
+        Revision(
+            2, Timestamp(2), "bbb",
+            "{{Стаття проекту Крим|важливість=найвища|рівень=ДС}}"
+        ),
+        Revision(
+            3, Timestamp(3), "ccc",
+            "{{Стаття проекту Крим|важливість=найвища|рівень=ВС}}"
+        ),
+            ]
     page = Page("Foobar", 1, revisions)
 
     observations = ukwiki.extract(page)
