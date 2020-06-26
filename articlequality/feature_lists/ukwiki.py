ab90b2b882d50dc7c6c1e43b33ba00a897356571
Chaitanya Mittal <chtnnh@gmail.com>
2020-06-26 23:11:18 +0400

    Placeholder commit to test feature_lists/ukwiki.py

--- articlequality/feature_lists/ukwiki.py
+++ articlequality/feature_lists/ukwiki.py
@@ -14,7 +14,7 @@ from revscoring.languages import ukrainian
 from . import wikipedia
 
 # Templates
-infobox_templates = wikipedia.infobox_templates()
+#infobox_templates = wikipedia.infobox_templates()
 CN_TEMPLATES = [
     r"Citation[_ ]needed",
     r"Cn",
@@ -29,14 +29,21 @@ MAIN_TEMPLATES = [
 main_article_templates = wikitext.revision.template_names_matching(
     "|".join(MAIN_TEMPLATES), name="ukwiki.revision.main_article_templates")
 CITE_TEMPLATES = [
+    r"Cite",
+    r"Harvard[_ ]citation[_ ]no[_ ]brackets", r"harvnb",
+    r"Harvard citation", r"harv",
+    r"Harvard citation text", r"harvtxt",
+    r"Harvcoltxt",
+    r"Harvcol",
+    r"Harvcolnb",
+    r"Harvard citations", r"harvs",
+    r"Harvp",
+    r"Citation"
 ]
 cite_templates = wikitext.revision.template_names_matching(
     "|".join(CITE_TEMPLATES), name="ukwiki.revision.cite_templates")
-SFN_TEMPLATES = [
-]
 shortened_footnote_templates = wikitext.revision.template_names_matching(
-    "|".join(SFN_TEMPLATES),
-    name="ukwiki.revision.shortened_footnote_templates")
+    "sfn", name="ukwiki.revision.shortened_footnote_templates")
 all_ref_tags = shortened_footnote_templates + wikitext.revision.ref_tags
 all_cite_templates = cite_templates + shortened_footnote_templates
 proportion_of_templates_references = \
@@ -49,19 +56,25 @@ non_cite_templates = sub(
 
 # Links
 CATEGORY_LINKS = [
+    r"Категорія",
+    r"Category",
+    r"Категория"
 ]
 category_links = wikitext.revision.wikilink_titles_matching(
     "|".join(CATEGORY_LINKS), name="ukwiki.revision.category_links")
 
 IMAGE_LINKS = [
+    r"File", 
+    r"Файл", 
+    r"Image", 
+    r"Зображення", 
+    r"Изображение"
 ]
 image_links = wikitext.revision.wikilink_titles_matching(
     "|".join(IMAGE_LINKS), name="ukwiki.revision.image_links")
 
-IMAGE_TEMPLATES = [
-]
 image_templates = wikitext.revision.template_names_matching(
-    "|".join(IMAGE_TEMPLATES), name="ukwiki.revision.image_template")
+    r"((Wide|Tall|scalable) image)|Panorama|Panorama 2", name="ukwiki.revision.image_template")
 
 
 def get_images(strs):
@@ -70,32 +83,29 @@ def get_images(strs):
     to get matching image patterns and returns a count of the matches.
     """
     matches = re.findall(
-        r"image[1-9]{1,2}|File:|Image:|photo[1-9][a-z]",
+        r"Зображення[1-9]{1,2}|Изображение:|Зображення:|Файл:|File:|Image:|photo[1-9][a-z]",
         "".join(strs), re.I)
     return len(matches)
 
 
-IMAGE_TEMPLATES_STR = [
-]
 image_templates_str = wikitext.revision.datasources.templates_str_matching(
-    "|".join(IMAGE_TEMPLATES_STR), name="ukwiki.revision.image_template_str")
+    r"{{(multiple image|image array|gallery|photomontage)",
+    name="ukwiki.revision.image_template_str")
 
 images_in_templates = Feature("ukwiki.revision.images_in_templates",
                               get_images,
                               depends_on=[image_templates_str],
                               returns=int)
 
-IMAGE_TAGS_STR = [
-]
 image_tags_str = wikitext.revision.datasources.tags_str_matching(
-    "|".join(IMAGE_TAGS_STR), name="ukwiki.revision.image_tags_str")
+    r"<(gallery|imagemap)", name="ukwiki.revision.image_tags_str")
 
 images_in_tags = Feature("ukwiki.revision.images_in_tags",
                          get_images,
                          depends_on=[image_tags_str],
                          returns=int)
 
-
+"""
 def get_infobox_images(strs):
     matches = re.findall(
          r"\.(jpg|jpeg|png|gif|svg|tiff|pdf|ogg|djvu)", "".join(strs), re.I)
@@ -108,9 +118,9 @@ infobox_images = Feature("ukwiki.infobox_images",
                          get_infobox_images,
                          depends_on=[infobox_templates_str],
                          returns=int)
-
+"""
 all_images = image_links + image_templates +\
-    images_in_templates + images_in_tags + infobox_images
+    images_in_templates + images_in_tags #+ infobox_images
 
 # References
 paragraphs = mappers.map(
@@ -118,15 +128,14 @@ paragraphs = mappers.map(
     name="ukwiki.revision.paragraphs"
 )
 paragraphs_without_refs = filters.regex_matching(
-    r"",  # Insert string to match ref tags
-    paragraphs, name="ukwiki.revision.paragraphs_without_refs"
+    r"^(?!\s*$)((?!<ref>)(.|\n))*$",
+    paragraphs,
+    name="ukwiki.revision.paragraphs_without_refs"
 )
 paragraphs_without_refs_total_length = aggregators.sum(
     mappers.map(len, paragraphs_without_refs),
-    name="ukwiki.revision.paragraph_without_refs_total_length"
+    name="ukwiki.revision.paragraphs_without_refs_total_length"
 )
-
-# Do we have words to watch for ukwiki?
 
 local_wiki = [
     all_images,
@@ -141,13 +150,11 @@ local_wiki = [
     non_templated_references / max(wikitext.revision.content_chars, 1),
     non_cite_templates,
     non_cite_templates / max(wikitext.revision.content_chars, 1),
-    infobox_templates,
+    # infobox_templates,
     cn_templates + 1,
     cn_templates / max(wikitext.revision.content_chars, 1),
     main_article_templates,
     main_article_templates / max(wikitext.revision.content_chars, 1),
-    (ukrainian.stemmed.revision.stem_chars /
-     max(wikitext.revision.content_chars, 1)),
     log(paragraphs_without_refs_total_length + 1),
 ]
 
