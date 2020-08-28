2650bf6aaa3ca6c5a47e78ea34b9b575c3603403
Ichi Rarámuri <pestanias@yahoo.com>
2020-08-28 04:47:50 -0500

    add docs to properties datatype list (#150)
    This list was automatically generated and then manually reformatted. This process is being documented so it can be repeated consistently and be improved upon.
    
    Currently, this documentation is in the only file of this type. Should this file be moved to its own directory at some point, it might make sense to move this documentation to its own README.md
    
    Bug: T261295

--- articlequality/feature_lists/property_datatypes.py
+++ articlequality/feature_lists/property_datatypes.py
@@ -1,3 +1,16 @@
+# Wikidata feature lists
+
+# To add/delete new properties from the `wikidatawiki.py` feature list we need to manually 
+# update this file. 
+
+# Currently the file contains a list of `NON_EXTERNAL_IDENTIFIERS` that we want to extract 
+# as features. `NON_EXTERNAL_IDENTIFIERS` is used instead of `EXTERNAL_IDENTIFIERS` because 
+# external identifiers comprise about 5000+ of the total 8000+ currently on the database. 
+# Using NON_EXTERNAL_IDENTIFIERS (~2500) reduces the processing time. 
+
+# This list was obtained by making this query: https://quarry.wmflabs.org/query/47645
+# and then doing a search and replace to format it as a python list.
+
 NONEXTERNAL_IDENTIFIERS = [
     'P10',
     'P14',
