2706c4baa5b1aa96e447dc38053c28fb66bb1432
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2020-08-28 10:11:07 +0200

    moved wikidata feature lists explanation

--- articlequality/feature_lists/property_datatypes.py
+++ articlequality/feature_lists/property_datatypes.py
@@ -1,3 +1,13 @@
+# Wikidata feature lists
+
+# To add/delete new properties from the `wikidatawiki.py` feature list we need to manually 
+# update this file. 
+
+# Currently the file contains a list of `NON_EXTERNAL_IDENTIFIERS` that we want to extract 
+# as features. `NON_EXTERNAL_IDENTIFIERS` is used instead of `EXTERNAL_IDENTIFIERS` because 
+# external identifiers comprise about 5000+ of the total 8000+ currently on the database. 
+#Using NON_EXTERNAL_IDENTIFIERS (~2500) reduces the processing time. 
+
 NONEXTERNAL_IDENTIFIERS = [
     'P10',
     'P14',
