7ffb5676ba8d33f6e877a8e1818a5979412207e2
Amir Sarabadani <Ladsgroup@gmail.com>
2020-09-08 19:18:56 +0200

    Add tests to ensure item parts are being counted correctly
    Bug: T261328

--- tests/feature_lists/test_wikidatawiki.py
+++ tests/feature_lists/test_wikidatawiki.py
@@ -10,6 +10,7 @@ from revscoring.features import wikibase as wikibase_
 from revscoring.features.meta import aggregators
 
 from articlequality.feature_lists import wikidatawiki
+from articlequality.feature_lists.wikibase import item
 
 present_properties = wikibase_.revision.datasources.properties
 suggested_properties = \
@@ -119,3 +120,33 @@ def test_has_commons_media(q7251, crab_nebula):
                  cache={entity: q7251}) is True
     assert solve(wikidatawiki.has_commons_media,
                  cache={entity: crab_nebula}) is False
+
+
+def test_entity_parts(q7251):
+    assert solve(wikibase_.revision.claims,
+                 cache={entity: q7251}) == 159
+    assert solve(wikibase_.revision.properties,
+                 cache={entity: q7251}) == 127
+    assert solve(wikibase_.revision.aliases,
+                 cache={entity: q7251}) == 24
+    assert solve(wikibase_.revision.sources,
+                 cache={entity: q7251}) == 123
+    assert solve(wikibase_.revision.qualifiers,
+                 cache={entity: q7251}) == 31
+    assert solve(wikibase_.revision.badges,
+                 cache={entity: q7251}) == 5
+    assert solve(wikibase_.revision.labels,
+                 cache={entity: q7251}) == 153
+    assert solve(wikibase_.revision.descriptions,
+                 cache={entity: q7251}) == 48
+
+    entity_features = list(solve(item, cache={entity: q7251}))
+    assert entity_features == [
+        5.075173815233827,
+        4.852030263919617,
+        3.2188758248682006,
+        4.820281565605037,
+        3.4657359027997265,
+        1.791759469228055,
+        5.0369526024136295,
+        3.8918202981106265]
