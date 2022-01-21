19cc5f294f417e5625af3173aac576bcede7898d
Sahethi <dgsahethi@gmail.com>
2022-01-22 00:58:13 +0530

    matching template regex till the end in trwiki.py

--- articlequality/feature_lists/trwiki.py
+++ articlequality/feature_lists/trwiki.py
@@ -9,7 +9,7 @@ from revscoring.features.modifiers import max, sub
 from . import wikipedia
 
 cite_templates = wikitext.revision.template_names_matching(
-    r"Kaynak|.*[ _]kaynağı",
+    r"Kaynak$|.*[ _]kaynağı$",
     name="trwiki.revision.cite_templates")
 proportion_of_templated_references = \
     cite_templates / max(wikitext.revision.ref_tags, 1)
@@ -19,17 +19,17 @@ non_cite_templates = sub(
     name="trwiki.revision.non_cite_templates"
 )
 infobox_templates = wikitext.revision.template_names_matching(
-    r".*[ _]bilgi[ _]kutusu",
+    r".*[ _]bilgi[ _]kutusu$",
     name="trwiki.revision.infobox_templates")
 
 # Copied (2015-10-29) from:
 # https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Citez_vos_sources#R.C3.A9clamation_et_contestation_de_sources
 cn_templates = wikitext.revision.template_names_matching(
-    r"Kaynak[ _]belirt|Olgu|Fact|Delil",
+    r"Kaynak[ _]belirt$|Olgu$|Fact$|Delil$",
     name="trwiki.revision.lvl1_cn_templates")
 
 main_article_templates = wikitext.revision.template_names_matching(
-    r"Ana|Anamadde",
+    r"Ana$|Anamadde$",
     name="trwiki.main_article_templates")
 
 # Links
